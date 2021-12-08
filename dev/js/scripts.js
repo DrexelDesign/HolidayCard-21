16ba314a472674a567c29ee3d6263ae6ecaf5b1c
Josh <josh@gdovindesigns.com>
2021-12-08 00:27:51 -0500

    working on project

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,11 @@
 import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 import { GSDevTools } from "gsap/GSDevTools";
-gsap.registerPlugin(GSDevTools);
+gsap.registerPlugin(GSDevTools,SplitText);
 
 gsap.set("#window-scene",{transformOrigin:"60% 40%"});
 gsap.set("#cabin-scene",{transformOrigin:"80% 50%"});
+gsap.set("#bg",{scaleY:2});
 
 
 var audio = new Audio('audio/cardMusic.mp3');
@@ -18,36 +20,72 @@ function scaleWindow(){
     return tl;
 }
 
-function scaleHouse(){
+function scaleOutside(){
     const tl = new gsap.timeline();
-    tl.to("#cabin-scene",{duration:60, ease:"none", scale:10},"zoom")
-    .from("#front-left",{duration: 10, ease:"none", y:"+=200"},"zoom")
-    .from("#front-right",{duration: 10, ease:"none", y:"+=200"},"zoom")
-    .from("#middle-in-front-of",{duration: 10, ease:"none", y:"+=300"},"zoom")
-    .from("#house-hill",{duration: 10, ease:"none", y:"+=300"},"zoom")
-    .from("#center",{duration: 10, ease:"none", y:"+=350"},"zoom")
-    .from("#background-mountains",{duration: 10, ease:"none", y:"+=400"},"zoom")
-    .from("#bg",{duration: 10, ease:"none", scaleY:2},"zoom");
-    return tl
+    tl.from("#front-left",{duration: 5, y:"+=200"},"zoom")
+    .from("#front-right",{duration: 5, y:"+=200"},"zoom")
+    .from("#middle-in-front-of",{duration: 5,  y:"+=250"},"zoom")
+    .from("#house-hill",{duration: 5,  y:"+=250"},"zoom")
+    .from("#center",{duration: 5,  y:"+=300"},"zoom")
+    .from("#background-mountains",{duration: 5,  y:"+=300"},"zoom")
+    // .from("#bg",{duration: 20, scaleY:2},"zoom");
+    return tl;
 }
 
-function zoomHouse(){
+function majorAniIn(){
+    var splitMajors = new SplitText("#majors li",{type: "words,chars"}),
+    chars = splitMajors.chars; 
     const tl = new gsap.timeline();
-    tl.to("#cabin-scene",{duration:20, ease:"none", scale:70, y:"-=800", x:"-=600"},"zoom");
-    return tl
+    tl.from("#majors",{duration:0.5, scale:0, alpha:0, xPercent:-50, yPercent:-50})
+    .from(chars,{duration:0.5, y:"+=50", stagger:0.01, alpha:0});
+    return tl;
 }
 
-function fadeOutHouse(){
+function majorAniOut(){
+    var splitMajors = new SplitText("#majors li",{type: "words"}),
+    chars = splitMajors.chars; 
     const tl = new gsap.timeline();
-    tl.to("#cabin-scene",{duration:5, alpha:0})
-    .to(".snow",{duration:0.25, alpha:0});
-    return tl
+    tl.to(chars,{duration:0.5, y:"+=50", stagger:-0.01, alpha:0})
+    .to("#majors",{duration:0.5, scale:0, alpha:0, xPercent:-50, yPercent:-50});
+    return tl;
 }
 
-mainTl.add(scaleWindow(),"scaleFirst")
-.add(scaleHouse(),"scaleFirst")
-.add(zoomHouse())
-.add(fadeOutHouse(),"-=15");
+
+
+
+// function scaleHouse(){
+//     const tl = new gsap.timeline();
+//     tl.to("#cabin-scene",{duration:60, ease:"none", scale:10},"zoom")
+//     .from("#front-left",{duration: 10, ease:"none", y:"+=200"},"zoom")
+//     .from("#front-right",{duration: 10, ease:"none", y:"+=200"},"zoom")
+//     .from("#middle-in-front-of",{duration: 10, ease:"none", y:"+=300"},"zoom")
+//     .from("#house-hill",{duration: 10, ease:"none", y:"+=300"},"zoom")
+//     .from("#center",{duration: 10, ease:"none", y:"+=350"},"zoom")
+//     .from("#background-mountains",{duration: 10, ease:"none", y:"+=400"},"zoom")
+//     .from("#bg",{duration: 20, ease:"none", scaleY:2},"zoom");
+//     return tl;
+// }
+
+// function zoomHouse(){
+//     const tl = new gsap.timeline();
+//     tl.to("#cabin-scene",{duration:20, ease:"none", scale:70, y:"-=800", x:"-=600"},"zoom");
+//     return tl
+// }
+
+// function fadeOutHouse(){
+//     const tl = new gsap.timeline();
+//     tl.to("#cabin-scene",{duration:5, alpha:0})
+//     .to(".snow",{duration:0.25, alpha:0});
+//     return tl
+// }
+
+mainTl.add(scaleWindow(),"zoom")
+.add(scaleOutside(),"zoom")
+.add(majorAniIn(),"-=50%")
+.add(majorAniOut());
+// .add(scaleHouse(),"scaleFirst")
+// .add(zoomHouse())
+// .add(fadeOutHouse(),"-=15");
 
 GSDevTools.create();
 
