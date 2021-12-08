7ad2e37f4a1ee7471de385d0c6b142f3b5e85c08
Josh <josh@gdovindesigns.com>
2021-12-08 07:44:35 -0500

    working on zoom

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,8 +6,6 @@ gsap.registerPlugin(GSDevTools,SplitText);
 gsap.set("#window-scene",{transformOrigin:"60% 40%"});
 gsap.set("#cabin-scene",{transformOrigin:"80% 50%"});
 gsap.set("#bg",{scaleY:2});
-// gsap.set("#chimney-main",{alpha:0});
-
 
 
 // var audio = new Audio('audio/cardMusic.mp3');
@@ -31,7 +29,6 @@ function scaleOutside(){
     .from("#center",{duration: 5,  y:"+=300"},"zoom")
     .from("#background-mountains",{duration: 5,  y:"+=300"},"zoom")
     .from("#chimney-main",{duration: 5, alpha:0, y:"+=150"},"-=4");
-    // .from("#bg",{duration: 20, scaleY:2},"zoom");
     return tl;
 }
 
@@ -50,6 +47,19 @@ function majorAniOut(){
     const tl = new gsap.timeline();
     tl.to(lines,{duration:0.5, y:"+=50", stagger:-0.01, alpha:0})
     .to("#majors",{duration:0.5, alpha:0},"-=50%");
+    return tl;
+}
+
+function zoomIn(){
+    const tl = new gsap.timeline();
+    tl.to("#front-left",{duration: 4, ease:"none", y:"+=800", scale:2.5,transformOrigin:"center"},"zoom")
+    .to("#front-right",{duration: 4, ease:"none", y:"+=800", scale:2.5,transformOrigin:"center"},"zoom")
+    .to("#middle-in-front-of",{duration: 4, ease:"none", y:"+=850", scale:2.5,transformOrigin:"center"},"zoom")
+    .to("#house-hill",{duration: 8, ease:"none", y:"+=800", scale:50,transformOrigin:"center", x:"-=1500"},"zoom")
+    .to("#center",{duration: 8, ease:"none", y:"+=400", scale:2,transformOrigin:"center"},"zoom")
+    .to("#background-mountains",{duration: 8, ease:"none", y:"+=200", scale:1.5,transformOrigin:"center"},"zoom")
+    .to(".background",{duration: 2, ease:"none", alpha:0},"-=6")
+    .to("#cabin-scene",{duration: 2, alpha:0},"-=2")
     return tl;
 }
 
@@ -85,10 +95,8 @@ function majorAniOut(){
 mainTl.add(scaleWindow(),"zoom")
 .add(scaleOutside(),"zoom")
 .add(majorAniIn(),"-=50%")
-.add(majorAniOut(),"+=50%");
-// .add(scaleHouse(),"scaleFirst")
-// .add(zoomHouse())
-// .add(fadeOutHouse(),"-=15");
+.add(majorAniOut(),"+=50%")
+.add(zoomIn());
 
 GSDevTools.create();
 
